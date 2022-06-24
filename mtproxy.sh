f98152c4eccc68f10ec846f143cd5fe3d11f002e
sudug <60267838+459816302@users.noreply.github.com>
2022-06-24 22:11:40 +0800

    Update mtproxy.sh

--- mtproxy.sh
+++ mtproxy.sh
@@ -95,14 +95,14 @@ install(){
 
   if [[ "`uname -m`" != "x86_64" ]];then
     if [ ! -d 'MTProxy' ];then
-      git clone https://github.com/TelegramMessenger/MTProxy
+      git clone https://github.com/459816302/MTProxy
     fi;
     cd MTProxy
     make && cd objs/bin
     cp -f $WORKDIR/MTProxy/objs/bin/mtproto-proxy $WORKDIR
     cd $WORKDIR
   else
-    wget https://github.com/ellermister/mtproxy/releases/download/0.02/mtproto-proxy -O mtproto-proxy -q
+    wget https://github.com/459816302/-mtproxy/releases/download/0.02/mtproto-proxy -O mtproto-proxy -q
     chmod +x mtproto-proxy
   fi
 }
