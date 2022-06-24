8baa55af4c191612300473096e7ce1f7a8637af4
sudug <60267838+expfukck@users.noreply.github.com>
2022-06-24 22:35:31 +0800

    Update mtproxy.sh

--- mtproxy.sh
+++ mtproxy.sh
@@ -95,14 +95,14 @@ install(){
 
   if [[ "`uname -m`" != "x86_64" ]];then
     if [ ! -d 'MTProxy' ];then
-      git clone https://github.com/459816302/MTProxy1
+      git clone https://github.com/expfukck/MTProxy1
     fi;
     cd MTProxy
     make && cd objs/bin
     cp -f $WORKDIR/MTProxy/objs/bin/mtproto-proxy $WORKDIR
     cd $WORKDIR
   else
-    wget https://github.com/459816302/-mtproxy/releases/download/0.02/mtproto-proxy -O mtproto-proxy -q
+    wget https://github.com/expfukck/-mtproxy/releases/download/0.02/mtproto-proxy -O mtproto-proxy -q
     chmod +x mtproto-proxy
   fi
 }
